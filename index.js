7f35f77cdd7e187dfe6c048d0b7dd3068fd1979e
Nathan Sculli <scull7@gmail.com>
2016-10-24 15:14:53 -0700

    export rabbitmq components.

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 
 const ExpressMySqlStore  = require('./express-mysql.js')
 const ExpressMemoryStore = require('./express-memory.js')
+const ExpressRabbitMQ    = require('./express-rabbitmq.js')
 const MySqlStore         = require('./lib/mysql')
 const MemoryStore        = require('./lib/memory')
+const RabbitMQFacade     = require('./lib/rabbitmq/facade.js')
+const RabbitMQPool       = require('./lib/rabbitmq/pool.js')
 
 
 module.exports = {
   Express: {
     MySqlStore: ExpressMySqlStore
   , MemoryStore: ExpressMemoryStore
+  , RabbitMQ: ExpressRabbitMQ
   }
 , MySqlStore
 , MemoryStore
+, RabbitMQ: {
+    Pool: RabbitMQPool
+  , Facade: RabbitMQFacade
+  }
 }
