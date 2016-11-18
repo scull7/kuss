b78dee6219df4f9d85ccb3d86c11ed5bb0d34548
Jonathan Lee <jlee@influential.co>
2016-11-17 20:52:36 -0800

    findWhereEq memory

--- test/lib/memory.spec.js
+++ test/lib/memory.spec.js
@@ -2,16 +2,18 @@
 /* eslint no-magic-numbers: 0 */
 /* eslint max-nested-callbacks: [ 'error', 5 ] */
 
-const { expect }  = require('chai');
-const MemoryStore = require('../../lib/memory');
+const R           = require('ramda')
+const { expect }  = require('chai')
+const MemoryStore = require('../../lib/memory')
+
 
 describe('lib/memory-store.js', function() {
 
-  let store = null;
+  let store = null
 
   beforeEach(function() {
-    store = MemoryStore();
-  });
+    store = MemoryStore()
+  })
 
 
   describe('::update', function() {
@@ -81,36 +83,36 @@ describe('lib/memory-store.js', function() {
 
     it('should insert a new value', function() {
 
-      const bucket = 'test';
-      const data   = { foo: 'bar', boo: 'fuck' };
-      const keys   = [ 'boo' ];
+      const bucket = 'test'
+      const data   = { foo: 'bar', boo: 'fuck' }
+      const keys   = [ 'boo' ]
 
       return store.upsert(bucket, keys, data)
 
         .then((x) => {
 
-          expect(x).to.be.a('string');
+          expect(x).to.be.a('string')
 
-          return store.getById(bucket, x);
+          return store.getById(bucket, x)
 
         })
 
         .then((x) => {
 
-          expect(x.foo).to.eql('bar');
-          expect(x.boo).to.eql('fuck');
+          expect(x.foo).to.eql('bar')
+          expect(x.boo).to.eql('fuck')
 
-        });
+        })
 
-    });
+    })
 
 
     it ('should update an existing value', function() {
 
-      const bucket = 'test';
-      const data   = { foo: 'random thing', bar_key: 'blah' };
-      const update = { foo: 'another thing', bar_key: 'blah' };
-      const keys   = [ 'bar_key' ];
+      const bucket = 'test'
+      const data   = { foo: 'random thing', bar_key: 'blah' }
+      const update = { foo: 'another thing', bar_key: 'blah' }
+      const keys   = [ 'bar_key' ]
 
       return store.insert(bucket, data)
 
@@ -120,20 +122,20 @@ describe('lib/memory-store.js', function() {
 
       .then((x) => {
 
-        expect(x.foo).to.eql('another thing');
-        expect(x.bar_key).to.eql('blah');
+        expect(x.foo).to.eql('another thing')
+        expect(x.bar_key).to.eql('blah')
 
-      });
+      })
 
-    });
+    })
 
     it ('should throw an error when invalid bucket', function() {
 
-      const bucket = 'test';
-      const other_bucket = 'nottest';
-      const data   = { foo: 'random thing', bar_key: 'blah' };
-      const update = { foo: 'another thing', bar_key: 'blah' };
-      const keys   = [ 'bar_key' ];
+      const bucket = 'test'
+      const other_bucket = 'nottest'
+      const data   = { foo: 'random thing', bar_key: 'blah' }
+      const update = { foo: 'another thing', bar_key: 'blah' }
+      const keys   = [ 'bar_key' ]
 
       return store.insert(bucket, data)
 
@@ -144,8 +146,8 @@ describe('lib/memory-store.js', function() {
 
         .then((x) => {
 
-          expect(x.foo).to.eql('another thing');
-          expect(x.bar_key).to.eql('blah');
+          expect(x.foo).to.eql('another thing')
+          expect(x.bar_key).to.eql('blah')
 
         })
 
@@ -153,23 +155,23 @@ describe('lib/memory-store.js', function() {
 
           // should throw an error for missing bucket
           (err.name).must.eql('Error');
-          (err.message).must.eql(`Invalid Bucket: ${other_bucket}`);
+          (err.message).must.eql(`Invalid Bucket: ${other_bucket}`)
 
-        });
+        })
 
-    });
+    })
 
-  });
+  })
 
   describe('::projectAll', function() {
 
     it('should return proper projected keys', function() {
 
-      const bucket = 'test';
-      const data1  = { foo: 'random thing', bar_key: 'blah' };
-      const data2  = { foo: 'another thing', bar_key: 'derp' };
-      const data3  = { foo: 'yet another thing', bar_key: 'foo' };
-      const keys   = [ 'bar_key' ];
+      const bucket = 'test'
+      const data1  = { foo: 'random thing', bar_key: 'blah' }
+      const data2  = { foo: 'another thing', bar_key: 'derp' }
+      const data3  = { foo: 'yet another thing', bar_key: 'foo' }
+      const keys   = [ 'bar_key' ]
 
       return store.insert(bucket, data1)
         .then(() => store.insert(bucket, data2))
@@ -179,91 +181,184 @@ describe('lib/memory-store.js', function() {
 
         .then((x) => {
 
-          x.must.have.length(3);
-          x[0].must.include('blah');
-          x[1].must.include('derp');
-          x[2].must.include('foo');
+          x.must.have.length(3)
+          x[0].must.include('blah')
+          x[1].must.include('derp')
+          x[2].must.include('foo')
 
-        });
+        })
 
-    });
+    })
 
-  });
+  })
 
   describe('::findOneBy', function() {
 
     it('should get one item by prop and value', function() {
 
-      const bucket = 'test';
-      const data1 = { foo: 'random', bar_key: 'blah' };
-      const data2 = { foo: 'randomer', bar_key: 'derp' };
-      const proj = [ 'bar_key' ];
+      const bucket = 'test'
+      const data1 = { foo: 'random', bar_key: 'blah' }
+      const data2 = { foo: 'randomer', bar_key: 'derp' }
+      const proj = [ 'bar_key' ]
 
       return store.insert(bucket, data1)
         .then(() => store.insert(bucket, data2))
-        
+
         .then(() => store.findOneBy(bucket, proj, 'bar_key', 'blah'))
 
         .then((x) => {
 
-          x.must.be.an.object();
-          x.must.include('blah');
+          x.must.be.an.object()
+          x.must.include('blah')
 
-        });
+        })
 
-    });
+    })
 
     it('should return an error if it returns more than one obj', function() {
 
-      const bucket = 'test';
-      const data1 = { foo: 'random', bar_key: 'blah' };
-      const data2 = { foo: 'randomer', bar_key: 'derp' };
-      const data3 = { foo: 'randomest', bar_key: 'derp' };
-      const proj = [ 'bar_key' ];
+      const bucket = 'test'
+      const data1 = { foo: 'random', bar_key: 'blah' }
+      const data2 = { foo: 'randomer', bar_key: 'derp' }
+      const data3 = { foo: 'randomest', bar_key: 'derp' }
+      const proj = [ 'bar_key' ]
 
       return store.insert(bucket, data1)
         .then(() => store.insert(bucket, data2))
         .then(() => store.insert(bucket, data3))
-        
+
         .then(() => store.findOneBy(bucket, proj, 'bar_key', 'derp'))
 
         .then((x) => {
-            
-          console.log(x);
+
+          console.log(x)
 
         })
         .catch((e) => {
 
-          (e.name).must.eql('TooManyRecords');
+          (e.name).must.eql('TooManyRecords')
 
-        }); 
+        })
 
-    });
+    })
 
-  });
+  })
 
-  
+
   describe('::findById', function() {
 
     it('should get one item by id and project', function() {
 
-      const bucket = 'test';
-      const data = { foo: 'random', bar_key: 'blah' };
-      const proj = [ 'bar_key' ];
+      const bucket = 'test'
+      const data = { foo: 'random', bar_key: 'blah' }
+      const proj = [ 'bar_key' ]
 
       return store.insert(bucket, data)
-        
+
         .then(store.findById(bucket, proj))
 
         .then((x) => {
 
-          x.must.be.an.object();
-          x.must.include('blah');
+          x.must.be.an.object()
+          x.must.include('blah')
 
-        });
+        })
 
-    });
+    })
 
-  });
+  })
 
-});
+
+  describe('::findWhereEq', function() {
+
+    const state = {
+      test : {
+        1  : { id : '1', foo : 'bar1' }
+      , 2  : { id : '2', foo : 'bar1' }
+      , 3  : { id : '3', foo : 'bar1' }
+      , 4  : { id : '4', foo : 'bar1' }
+      , 5  : { id : '5', foo : 'bar1' }
+      , 6  : { id : '6', foo : 'bar1' }
+      , 7  : { id : '7', foo : 'bar1' }
+      , 8  : { id : '8', foo : 'bar1' }
+      , 9  : { id : '9', foo : 'bar1' }
+      , 10 : { id : '10', foo : 'bar10' }
+      , 11 : { id : '11', foo : 'bar11' }
+      , 12 : { id : '12', foo : 'bar12' }
+      , 13 : { id : '13', foo : 'bar13' }
+      , 14 : { id : '14', foo : 'bar14' }
+      , 15 : { id : '15', foo : 'bar15' }
+      }
+    }
+
+    const tempStore = MemoryStore(state)
+
+
+    it('should get all objects matching the predicate', function() {
+
+      const request = {
+        projection : ['id', 'foo']
+      , predicates : {
+          foo : 'bar1'
+        }
+      , sort : { foo : 'desc' }
+      }
+
+      return tempStore.findWhereEq('test', request)
+
+      .then(rows => { rows.must.have.length(9) })
+
+    })
+
+
+    it('should skip and limit the results', function() {
+
+      const request = {
+        projection : ['id', 'foo']
+      , limit : 3
+      , skip : 5
+      }
+
+      const expected = [
+        { id : '6', foo : 'bar1' }
+      , { id : '7', foo : 'bar1' }
+      , { id : '8', foo : 'bar1' }
+      ]
+
+      return tempStore.findWhereEq('test', request)
+
+      .then(rows => {
+        (R.equals(expected, rows)).must.be.true()
+        rows.must.have.length(3)
+      })
+
+    })
+
+
+    it('should sort the results', function() {
+
+      const request = {
+        projection : ['id', 'foo']
+      , sort       : { foo : 'desc' }
+      , skip       : 0
+      , limit      : 3
+      }
+
+      const expected = [
+        { id : '15', foo : 'bar15' }
+      , { id : '14', foo : 'bar14' }
+      , { id : '13', foo : 'bar13' }
+      ]
+
+      return tempStore.findWhereEq('test', request)
+
+      .then(rows => {
+        (R.equals(expected, rows)).must.be.true()
+        rows.must.have.length(3)
+      })
+
+    })
+
+  })
+
+})
