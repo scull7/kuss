e4c6153f3640b200e09990566c824e57a361d681
Jonathan Lee <jlee@influential.co>
2016-11-29 17:12:44 -0800

    unnecessary function

--- test/lib/error.js
+++ test/lib/error.js
@@ -8,24 +8,6 @@ const Error  = require('../../lib/error.js')
 
 describe('lib/error.js', function() {
 
-
-  it('You should be able to throw a NotFound error',
-  function() {
-
-    try {
-
-      Error.throwCussError(Error.NotFound, 'couch', 'foo', 'bar')
-
-    } catch (e) {
-
-      demand(e.name).eql('NotFound')
-      demand(e.status).eql(404)
-      demand(e.message).eql(`Not Found - STORE: couch TABLE: foo ID: bar`)
-
-    }
-
-  })
-
   it('NotFound Error should have a composable throw method',
   function() {
 
